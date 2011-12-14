1ea8e30d9e21940e4fa854cc18693c7287572efd
Misha Doronin <misha@doronin.org>
2011-12-14 19:24:58 +0100

    Implemented the nodes list page.

---
+++ portal/src/hot/org/vamdc/portal/session/Nodes.java
@@ -0,0 +1,89 @@
+package org.vamdc.portal.session;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import net.ivoa.xml.voresource.v1.Resource;
+
+import org.jboss.seam.ScopeType;
+import org.jboss.seam.annotations.Logger;
+import org.jboss.seam.annotations.Name;
+import org.jboss.seam.annotations.Scope;
+import org.jboss.seam.log.Log;
+import org.vamdc.portal.registry.Client;
+import org.vamdc.registry.client.Registry;
+import org.vamdc.registry.client.Registry.Service;
+import org.vamdc.registry.client.RegistryCommunicationException;
+
+@Name("nodes")
+@Scope(ScopeType.STATELESS)
+public class Nodes
+{
+    @Logger private Log log;
+
+    private Registry registry;
+
+    public Nodes(){
+    	this.registry=Client.INSTANCE.get();
+    }
+    
+    public final class Node{
+    	private final String ivoaID;
+    	private Resource node;
+    	
+    	private Node(String id){
+    		this.ivoaID = id;
+    		try {
+				this.node=registry.getResourceMetadata(ivoaID);
+			} catch (RegistryCommunicationException e) {
+				this.node=null;
+			}
+    	}
+    	
+    	public String getName(){
+    		if (node!=null)
+				return node.getTitle();
+    		return ivoaID;
+    	}
+    	public String getDescription(){
+    		if (node!=null && node.getContent()!=null)
+				return node.getContent().getDescription();
+    		return ivoaID;
+    	}
+    	
+    	public String getMaintainer(){
+    		if (node!=null && node.getCuration()!=null && node.getCuration().getContact()!=null)
+				return node.getCuration().getContact().get(0).getEmail();
+			return ivoaID;
+    	}
+    	
+    	public String getStatus(){
+    		return "Unknown";
+    	}
+    	
+    }
+
+    public List<Node> getList(){
+    	List<Node> result = new ArrayList<Node>();
+    	
+    	if (registry==null)
+    		return result;
+    	
+    	Collection<String> ivoaIDs=null;
+		try {
+			ivoaIDs = registry.getIVOAIDs(Service.VAMDC_TAP);
+		} catch (RegistryCommunicationException e) {
+		}
+    	
+		if (ivoaIDs==null)
+    		return result;
+    	
+		for (String id:ivoaIDs){
+    		result.add(new Node(id));
+    	}
+    	
+		return result;
+    }
+  
+}
