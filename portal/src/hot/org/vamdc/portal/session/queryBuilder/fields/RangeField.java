0e5fd5c26b02b4135192406745f6030b7f5bc45c
Misha Doronin <misha@doronin.org>
2012-02-08 18:45:47 +0100

    Fixed RangeField as suggested by Dugan

--- portal/src/hot/org/vamdc/portal/session/queryBuilder/fields/RangeField.java
+++ portal/src/hot/org/vamdc/portal/session/queryBuilder/fields/RangeField.java
@@ -7,7 +7,7 @@ public class RangeField extends AbstractField{
 	public RangeField(Restrictable keyword, String title) {
 		super(keyword, title);
 	}
-	
+
 	public RangeField(String prefix, Restrictable keyword, String title) {
 		super(keyword,title);
 		if (fieldIsSet(prefix))
@@ -20,10 +20,10 @@ public class RangeField extends AbstractField{
 	private String prefix="";
 	protected String loValue;
 	protected String hiValue;
-	
+
 	public String getHiValue(){	return hiValue; }
 	public String getLoValue(){	return loValue; }
-	
+
 	public void setLoValue(String loValue){
 		this.loValue = loValue;
 	}
@@ -31,27 +31,31 @@ public class RangeField extends AbstractField{
 	public void setHiValue(String hiValue){
 		this.hiValue = hiValue;
 	}
-	
+
 	public void setPrefix(String prefix){ this.prefix = prefix; }
-	
+
 	@Override
 	public String getQuery(){
 		fixCompareOrder();
-		
-		String result = getQueryPart(this.keyword.name(),">",loValue);
-		if (result.length()>0)
-			result+=" AND ";
-		result+=getQueryPart(this.keyword.name(),"<",hiValue);
+		String result = "";
+		if (hiValue.equals(loValue)){
+			result=getQueryPart(this.keyword.name(),"=",loValue);
+		}else{
+			result=getQueryPart(this.keyword.name(),">=",loValue);
+			if (result.length()>0)
+				result+=" AND ";
+			result+=getQueryPart(this.keyword.name(),"<=",hiValue);
+		}
 		return result;
-		
+
 	}
-	
+
 	private String getQueryPart(String keyword,String compare, String value){
 		if (fieldIsSet(value))
 			return prefix+keyword+" "+compare+" "+value;
 		return "";
 	}
-	
+
 	protected void fixCompareOrder(){
 		if (fieldIsSet(hiValue) && fieldIsSet(loValue)){
 			Double lo=Double.NaN;
@@ -60,9 +64,9 @@ public class RangeField extends AbstractField{
 				lo = Double.parseDouble(loValue);
 				hi = Double.parseDouble(hiValue);
 			}catch(NumberFormatException e){
-				
+
 			}
-			
+
 			if (lo>hi){
 				String tmp = loValue;
 				loValue=hiValue;
@@ -70,16 +74,16 @@ public class RangeField extends AbstractField{
 			}
 		}
 	}
-	
+
 	@Override
 	public boolean hasValue(){
 		return (fieldIsSet(hiValue)|| fieldIsSet(loValue));
 	}
-	
+
 	@Override
 	public void clear(){
 		hiValue="";
 		loValue="";
 	}
-	
+
 }
