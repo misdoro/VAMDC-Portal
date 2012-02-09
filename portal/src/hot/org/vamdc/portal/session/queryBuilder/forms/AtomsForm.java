088503e40b2b5ea2b4a99fc14699a889818d5689
Misha Doronin <misha@doronin.org>
2012-02-09 18:13:04 +0100

    Added unit conversion to AtomsForm energy

--- portal/src/hot/org/vamdc/portal/session/queryBuilder/forms/AtomsForm.java
+++ portal/src/hot/org/vamdc/portal/session/queryBuilder/forms/AtomsForm.java
@@ -7,6 +7,8 @@ import org.vamdc.portal.session.queryBuilder.QueryData;
 import org.vamdc.portal.session.queryBuilder.fields.AbstractField;
 import org.vamdc.portal.session.queryBuilder.fields.RangeField;
 import org.vamdc.portal.session.queryBuilder.fields.SimpleField;
+import org.vamdc.portal.session.queryBuilder.fields.UnitConvRangeField;
+import org.vamdc.portal.session.queryBuilder.unitConv.EnergyUnitConverter;
 
 public class AtomsForm extends AbstractForm implements Form {
 
@@ -22,7 +24,8 @@ public class AtomsForm extends AbstractForm implements Form {
 		fields.add(new RangeField(Restrictable.AtomMassNumber,"Mass number"));
 		fields.add(new RangeField(Restrictable.AtomNuclearCharge,"Nuclear charge"));
 		fields.add(new RangeField(Restrictable.IonCharge,"Ion charge"));
-		fields.add(new RangeField(Restrictable.StateEnergy,"StateEnergy"));
+		fields.add(new UnitConvRangeField(
+				Restrictable.StateEnergy, "Upper state energy", new EnergyUnitConverter()));
 	}
 
 }
