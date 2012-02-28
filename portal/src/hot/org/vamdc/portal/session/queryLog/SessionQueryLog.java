631a77dfb38923786c6f7941d0360aa3460b73a0
Misha Doronin <misha@doronin.org>
2012-02-28 20:27:10 +0100

    user SynchronizedList

--- portal/src/hot/org/vamdc/portal/session/queryLog/SessionQueryLog.java
+++ portal/src/hot/org/vamdc/portal/session/queryLog/SessionQueryLog.java
@@ -15,7 +15,7 @@ import org.vamdc.portal.entity.query.Query;
 public class SessionQueryLog implements Serializable{
 
 	private static final long serialVersionUID = 2555499604910810323L;
-	private List<Query> storedQueries = new ArrayList<Query>();
+	private List<Query> storedQueries = Collections.synchronizedList(new ArrayList<Query>());
 	private int idCounter=0;
 
 
@@ -31,21 +31,17 @@ public class SessionQueryLog implements Serializable{
 		
 		query.setQueryID(idCounter++);
 
-		synchronized(storedQueries){
-			this.storedQueries.add(query);
-		}
+		storedQueries.add(query);
+		
 	}
 
 	public void clear(){
-		synchronized(storedQueries){
-			storedQueries.clear();
-		}
+		storedQueries.clear();
 	}
 
 
 	public void delete(Integer queryId) {
 		Query toRemove = null;
-		synchronized(storedQueries){
 			for (Query stored:storedQueries){
 				if (stored.getQueryID()==queryId){
 					toRemove=stored;
@@ -53,7 +49,7 @@ public class SessionQueryLog implements Serializable{
 				}
 			}
 			storedQueries.remove(toRemove);
-		}
+		
 	}
 
 }
