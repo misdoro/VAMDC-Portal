93334309cffd8749c2bb512f2559897a6c04ce7f
Misha Doronin <misha@doronin.org>
2012-03-09 17:41:07 +0100

    Refactored settings to use context-specific prefix

--- portal/src/hot/org/vamdc/portal/Settings.java
+++ portal/src/hot/org/vamdc/portal/Settings.java
@@ -1,5 +1,7 @@
 package org.vamdc.portal;
 
+import org.jboss.seam.contexts.ServletLifecycle;
+
 /**
  * Various configuration properties names and their default values.
  * @author doronin
@@ -9,10 +11,7 @@ public enum Settings {
 
 	HTTP_CONNECT_TIMEOUT("HTTPConnTimeout", "2000"),
 	HTTP_DATA_TIMEOUT("HTTPDataTimeout","30000"),
-	STATIC_STORAGE_PATH("fileStoragePath","/tmp"),
-	STATIC_ACCESS_URL("fileStorageURL","http://localhost/"),
-	DISPLAY_URL("displayURL","http://localhost/"),
-	REGISTRY_URL("registryURL","http://casx019-zone1.ast.cam.ac.uk//registry/services/RegistryQueryv1_0"),
+	REGISTRY_URL("registryURL","http://casx019-zone1.ast.cam.ac.uk/registry/services/RegistryQueryv1_0"),
 	REGISTRY_UPDATE_INTERVAL("registryUpdateInterval","300000"),
 	AVAILABILITY_MONITOR_INTERVAL("availabilityMonitorInterval","300000"),
 	REGISTRY_RETRY_INTERVAL("registryRetryInterval","10000"),
@@ -32,13 +31,28 @@ public enum Settings {
 	public final String get(){
 		if (value!=null)
 			return value;
-		return (value=System.getProperty(this.key, this.defValue));
+		return loadValue();
 	}
 	
 	public final int getInt(){
 		if (value!=null)
 			return Integer.valueOf(value);
-		return Integer.valueOf(value=System.getProperty(this.key, this.defValue));
+		return Integer.valueOf(loadValue());
+	}
+	
+	private final String loadValue(){
+		String specificValue="";
+		String path = ServletLifecycle.getServletContext().getContextPath();
+		if (path!=null && path.length()>1){
+			path=path.substring(1);
+			specificValue = System.getProperty(path+"."+this.key);
+		}
+		
+		if (specificValue!=null && specificValue.length()>0)
+			this.value = specificValue;
+		else
+			this.value = System.getProperty(this.key, this.defValue);
+		return this.value;
 	}
 	
 }
