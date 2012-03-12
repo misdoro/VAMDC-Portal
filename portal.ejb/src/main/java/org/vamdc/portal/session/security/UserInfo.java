4e12aa5c0b80f58442e243e3b08a7aea9a7f42c2
Misha Doronin <misha@doronin.org>
2012-03-12 18:29:56 +0100

    Changed to ear web archive structure, not yet working (dependency issues)

---
+++ portal.ejb/src/main/java/org/vamdc/portal/session/security/UserInfo.java
@@ -0,0 +1,37 @@
+package org.vamdc.portal.session.security;
+
+import javax.persistence.EntityManager;
+
+import org.jboss.seam.ScopeType;
+import org.jboss.seam.annotations.AutoCreate;
+import org.jboss.seam.annotations.In;
+import org.jboss.seam.annotations.Name;
+import org.jboss.seam.annotations.Scope;
+import org.jboss.seam.security.Credentials;
+import org.jboss.seam.security.Identity;
+import org.vamdc.portal.entity.security.User;
+
+@Name("auth")
+@Scope(ScopeType.STATELESS)
+@AutoCreate
+public class UserInfo {
+	
+	@In private EntityManager entityManager;
+	@In private Credentials credentials;
+	@In private Identity identity;
+	
+	public User getUser(){
+		if (identity!=null){
+			System.out.println("identity try login: "+identity.tryLogin());
+			
+		}
+		
+		String user = null;
+		if (identity!=null && identity.isLoggedIn() && credentials!=null)
+			user=credentials.getUsername();
+		if (user!=null && user.length()>0){
+			return (User) entityManager.createQuery("from User where username =:username").setParameter("username", user).getSingleResult();
+		}
+		return null;
+	}
+}
