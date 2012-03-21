c3c09b4c9ccc2e9562dba659b73666856700a6b5
Misha Doronin <misha@doronin.org>
2012-03-21 12:56:57 +0100

    Use textField for inchikeys in atoms

--- portal.ejb/src/main/java/org/vamdc/portal/session/queryBuilder/forms/AtomsForm.java
+++ portal.ejb/src/main/java/org/vamdc/portal/session/queryBuilder/forms/AtomsForm.java
@@ -3,6 +3,7 @@ package org.vamdc.portal.session.queryBuilder.forms;
 import org.vamdc.dictionary.Restrictable;
 import org.vamdc.portal.session.queryBuilder.fields.RangeField;
 import org.vamdc.portal.session.queryBuilder.fields.SimpleField;
+import org.vamdc.portal.session.queryBuilder.fields.TextField;
 import org.vamdc.portal.session.queryBuilder.fields.UnitConvRangeField;
 import org.vamdc.portal.session.queryBuilder.unitConv.EnergyUnitConverter;
 
@@ -21,10 +22,10 @@ public class AtomsForm extends AbstractForm implements Form {
 		super();
 		
 		addField(new SimpleField(Restrictable.AtomSymbol,"Atom symbol"));
-		addField(new SimpleField(Restrictable.InchiKey,"InChIKey"));
 		addField(new RangeField(Restrictable.AtomMassNumber,"Mass number"));
 		addField(new RangeField(Restrictable.AtomNuclearCharge,"Nuclear charge"));
 		addField(new RangeField(Restrictable.IonCharge,"Ion charge"));
+		addField(new TextField(Restrictable.InchiKey,"InChIKey"));
 		addField(new UnitConvRangeField(
 				Restrictable.StateEnergy, "State energy", new EnergyUnitConverter()));
 	}
