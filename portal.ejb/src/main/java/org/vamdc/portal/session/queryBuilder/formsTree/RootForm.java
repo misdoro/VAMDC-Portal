7fa6b1922a726fb3ede7664fb5a961208411b340
Misha Doronin <misha@doronin.org>
2014-04-02 00:40:00 +0200

    add dummy getters for the interactive elements

--- portal.ejb/src/main/java/org/vamdc/portal/session/queryBuilder/formsTree/RootForm.java
+++ portal.ejb/src/main/java/org/vamdc/portal/session/queryBuilder/formsTree/RootForm.java
@@ -1,10 +1,30 @@
 package org.vamdc.portal.session.queryBuilder.formsTree;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class RootForm extends TreeForm{
 
+	private String selectedMode;
+	
+	List<String> modeOptions = new ArrayList<String>(){{
+		add("For collisional process");
+		add("For radiative process");
+		add("By species");
+	}};
+	
 	@Override
 	public void showNext() {
 		
+	}
+
+	public String getSelectedMode() {
+		return selectedMode;
+	}
+
+	public void setSelectedMode(String selectedMode) {
+		this.selectedMode = selectedMode;
+		System.out.println(selectedMode);
 	}
 
 }
