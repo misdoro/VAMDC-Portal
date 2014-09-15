0c1a26f16a19d583bfa62c878f0e8139a79b4450
Misha Doronin <misha@doronin.org>
2014-09-16 00:31:55 +0200

    Add test for complex methods of the RegistryFacade

---
+++ portal.ejb/src/test/java/org/vamdc/portal/registry/RegistryFacadeTest.java
@@ -0,0 +1,72 @@
+package org.vamdc.portal.registry;
+
+import static org.junit.Assert.*;
+
+import java.net.URL;
+import java.util.Collection;
+
+import org.junit.Test;
+
+public class RegistryFacadeTest {
+	private static RegistryFacade facade;
+	static{
+		System.out.println("Init the regisry...");
+		facade=new RegistryFacade();
+		facade.forceUpdate();
+		System.out.println("The registry is updated.");
+	}
+	
+	/**
+	 * Test that we can get resource titles for all registered nodes.
+	 */
+	@Test
+	public void testGetResourceTitle() {
+		Collection<String> ivoaids=facade.getTapIvoaIDs();
+		assertNotNull(ivoaids);
+		assertTrue(ivoaids.size()>0);
+		for (String ivoaid:ivoaids){
+			String title=facade.getResourceTitle(ivoaid);
+			assertNotNull(title);
+			assertTrue(title.length()>0);
+		}
+	}
+
+	@Test
+	public void testGetResourceDescription() {
+		Collection<String> ivoaids=facade.getTapIvoaIDs();
+		assertNotNull(ivoaids);
+		assertTrue(ivoaids.size()>0);
+		for (String ivoaid:ivoaids){
+			String descr=facade.getResourceDescription(ivoaid);
+			assertNotNull(descr);
+			assertTrue(descr.length()>0);
+		}
+	}
+
+	@Test
+	public void testGetNumberOfInputs() {
+		Collection<String> ivoaids=facade.getConsumerIvoaIDs();
+		assertNotNull(ivoaids);
+		assertTrue(ivoaids.size()>0);
+		for (String ivoaid:ivoaids){
+			Integer numInputs=facade.getConsumerNumberOfInputs(ivoaid);
+			assertNotNull(numInputs);
+			assertTrue(numInputs>0);
+		}
+	}
+
+	@Test
+	public void testGetConsumerServiceURL() {
+		Collection<String> ivoaids=facade.getConsumerIvoaIDs();
+		assertNotNull(ivoaids);
+		assertTrue(ivoaids.size()>0);
+		for (String ivoaid:ivoaids){
+			URL serviceURL=facade.getConsumerServiceURL(ivoaid);
+			assertNotNull(serviceURL);
+			assertTrue(serviceURL.getProtocol().contains("http"));
+			System.out.println(serviceURL.toString());
+			//Can't really check the serviceURL, they can be any valid HTTP URLs.
+		}
+	}
+
+}
