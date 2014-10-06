25e8c341309fdc153998cd279b186a97d70d5d53
Misha Doronin <misha@doronin.org>
2014-10-06 22:14:48 +0200

    add a working list of choices to the rootform

---
+++ portal.ejb/src/main/java/org/vamdc/portal/session/queryBuilder/formsTree/RadiativeForm.java
@@ -0,0 +1,15 @@
+package org.vamdc.portal.session.queryBuilder.formsTree;
+
+public class RadiativeForm extends TreeForm{
+
+	@Override
+	public String getView() {
+		return null;
+	}
+
+	@Override
+	public void showNext() {
+		
+	}
+
+}
--- portal.ejb/src/main/java/org/vamdc/portal/session/queryBuilder/formsTree/RootForm.java
+++ portal.ejb/src/main/java/org/vamdc/portal/session/queryBuilder/formsTree/RootForm.java
@@ -2,30 +2,44 @@ package org.vamdc.portal.session.queryBuilder.formsTree;
 
 import java.util.ArrayList;
 import java.util.List;
+import javax.faces.model.SelectItem;
 
 public class RootForm extends TreeForm{
 
-	private String selectedMode;
 	
-	List<String> modeOptions = new ArrayList<String>(){
-		private static final long serialVersionUID = -8406069388034760380L;
+	private NextForm selectedMode;
+	private enum NextForm{
+		collision,
+		radiative,
+		species
+	}
+	
+	private List<SelectItem> nextOptions = new ArrayList<SelectItem>(){/**
+		 * 
+		 */
+		private static final long serialVersionUID = 1L;
 
 	{
-		add("For collisional process");
-		add("For radiative process");
-		add("By species");
-	}};
+		add(new SelectItem(NextForm.collision,"For collisional process"));
+		add(new SelectItem(NextForm.radiative,"For radiative process"));
+		add(new SelectItem(NextForm.species,"By species"));
+	}
+	};
 	
 	@Override
 	public void showNext() {
 		
 	}
 
-	public String getSelectedMode() {
+	public List<SelectItem> getNextOptions(){
+		return nextOptions;
+	}	
+	
+	public NextForm getSelectedMode() {
 		return selectedMode;
 	}
 
-	public void setSelectedMode(String selectedMode) {
+	public void setSelectedMode(NextForm selectedMode) {
 		this.selectedMode = selectedMode;
 		System.out.println(selectedMode);
 	}
