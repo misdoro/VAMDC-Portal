1735115d8816b8dfd2bffe2123937a4db906eb79
Misha Doronin <misha@doronin.org>
2014-11-01 23:50:24 +0100

    Use unit-conversion range field for wavelength

--- portal.ejb/src/main/java/org/vamdc/portal/session/queryBuilder/formsTree/RadiativeForm.java
+++ portal.ejb/src/main/java/org/vamdc/portal/session/queryBuilder/formsTree/RadiativeForm.java
@@ -7,7 +7,13 @@ import java.util.List;
 import org.vamdc.dictionary.Restrictable;
 import org.vamdc.portal.session.queryBuilder.QueryTreeInterface;
 import org.vamdc.portal.session.queryBuilder.fields.AbstractField;
-import org.vamdc.portal.session.queryBuilder.fields.RangeField;
+import org.vamdc.portal.session.queryBuilder.fields.ProxyRangeField;
+import org.vamdc.portal.session.queryBuilder.fields.UnitConvRangeField;
+import org.vamdc.portal.session.queryBuilder.unitConv.CustomConverters;
+import org.vamdc.portal.session.queryBuilder.unitConv.EnergyUnitConverter;
+import org.vamdc.portal.session.queryBuilder.unitConv.FrequencyUnitConverter;
+import org.vamdc.portal.session.queryBuilder.unitConv.WavelengthUnitConverter;
+import org.vamdc.portal.session.queryBuilder.unitConv.WavenumberUnitConverter;
 
 public class RadiativeForm extends TreeForm{
 
@@ -15,7 +21,8 @@ public class RadiativeForm extends TreeForm{
 	public RadiativeForm(QueryTreeInterface tree) {
 		super(tree);
 		this.fields = new ArrayList<AbstractField>();
-		fields.add(new RangeField(Restrictable.RadTransWavelength, "Transition wavelength"));
+		ProxyRangeField wlField = setupWLField();
+		fields.add(wlField);
 	}
 	
 	public Collection<AbstractField> getFields() { return fields; }
@@ -30,4 +37,12 @@ public class RadiativeForm extends TreeForm{
 		
 	}
 
+	static ProxyRangeField setupWLField() {
+		ProxyRangeField wlField = new ProxyRangeField(Restrictable.RadTransWavelength,"Wavelength", new WavelengthUnitConverter());
+		wlField.addProxyField(new UnitConvRangeField(Restrictable.RadTransFrequency,"Frequency",new FrequencyUnitConverter()), CustomConverters.MHzToWl());
+		wlField.addProxyField(new UnitConvRangeField(Restrictable.RadTransWavenumber,"Wavenumber",new WavenumberUnitConverter()), CustomConverters.WnToWl());
+		wlField.addProxyField(new UnitConvRangeField(Restrictable.RadTransEnergy,"Energy",new EnergyUnitConverter()), CustomConverters.WnToWl());
+		return wlField;
+	}
+	
 }
